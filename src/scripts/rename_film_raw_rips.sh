3184bc6e74a53f4a1a934fdf420f447d111e78cd
Laurence Woodward <woodylaurence90@gmail.com>
2017-12-09 13:56:48 +0000

    Fixed problem where the film folder name wasn't parsed by the regex because the folder names had '/' at the end.

--- src/scripts/rename_film_raw_rips.sh
+++ src/scripts/rename_film_raw_rips.sh
@@ -18,8 +18,8 @@ do
 		continue;
 	fi
 
-		FILM_NAME_REGEX="^(.*)_(DVD|BluRay)Rip$"
-		filmName=$(sed -nr "s/$FILM_NAME_REGEX/\1/p" <<< "$filmFolder")
+		FILM_NAME_REGEX="^(.*)_(DVD|BluRay)Rip/$"
+		filmName=$(sed -nr "s|$FILM_NAME_REGEX|\1|p" <<< "$filmFolder")
 		if [[ -z "$filmName" ]]; then
 		echo "Woah, could not get film name from $filmFolder based on regex...Skipping."
 		continue
