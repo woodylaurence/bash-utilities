f2267d6de24ff776cda0010a5a04a214cabc61e6
Laurence Woodward <woodylaurence90@gmail.com>
2017-12-09 13:59:23 +0000

    Used different separator for sed command as '|' wasn't working.

--- src/scripts/rename_film_raw_rips.sh
+++ src/scripts/rename_film_raw_rips.sh
@@ -19,7 +19,7 @@ do
 	fi
 
 		FILM_NAME_REGEX="^(.*)_(DVD|BluRay)Rip/$"
-		filmName=$(sed -nr "s|$FILM_NAME_REGEX|\1|p" <<< "$filmFolder")
+		filmName=$(sed -nr "s%$FILM_NAME_REGEX%\1%p" <<< "$filmFolder")
 		if [[ -z "$filmName" ]]; then
 		echo "Woah, could not get film name from $filmFolder based on regex...Skipping."
 		continue
